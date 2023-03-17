a13cdf82cd1c9d3a44f5779416ce8a8ab57fd678
Sagi Sarussi <sagis@sagis.dev>
2023-03-17 14:39:45 +0200

    WIP - Poetry integration

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Application packaged using poetry2nix";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          kubeget = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.kubeget;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ poetry2nix.packages.${system}.poetry ];
+        };
+      });
+}
