9c2579c0ceb7d35d6d8aac58843125416bb41491
Sagi Sarussi <sagis@sagis.dev>
2023-03-18 21:40:41 +0200

    Add Alejandra as formatter

--- flake.nix
+++ flake.nix
@@ -5,47 +5,54 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        poetry_env = pkgs.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-          preferWheels = true;
-          python = pkgs.python310;
-        };
-        app = pkgs.poetry2nix.mkPoetryApplication ({
-          projectDir = ./.;
-          preferWheels = true;
-          python = pkgs.python310;
-        });
-        ociImage = pkgs.dockerTools.buildLayeredImage {
-          name = "kubeget";
-          tag = "0.2.0";
-          contents = [
-            app.dependencyEnv
-            pkgs.awscli2
-            pkgs.bash
-            pkgs.busybox
-            pkgs.kubectl
-          ];
-          config.Cmd = [ "/bin/bash" ];
-          maxLayers = 120;
-          created = "now";
-          # copyToRoot = [ pkgs.dockerTools.usrBinEnv pkgs.dockerTools.binSh ];
-        };
-      in
-      {
-        packages = { default = ociImage; image = ociImage; app = app; };
-        devShells.default = poetry_env.env.overrideAttrs (old: {
-          buildInputs = [
-            app
-            pkgs.awscli2
-            pkgs.kubectl
-            pkgs.podman
-            pkgs.poetry
-          ];
-        });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      poetry_env = pkgs.poetry2nix.mkPoetryEnv {
+        projectDir = ./.;
+        preferWheels = true;
+        python = pkgs.python310;
+      };
+      app = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+        preferWheels = true;
+        python = pkgs.python310;
+      };
+      ociImage = pkgs.dockerTools.buildLayeredImage {
+        name = "kubeget";
+        tag = "0.2.0";
+        contents = [
+          app.dependencyEnv
+          pkgs.awscli2
+          pkgs.bash
+          pkgs.busybox
+          pkgs.kubectl
+        ];
+        config.Cmd = ["/bin/bash"];
+        maxLayers = 120;
+        created = "now";
+        # copyToRoot = [ pkgs.dockerTools.usrBinEnv pkgs.dockerTools.binSh ];
+      };
+    in {
+      packages = {
+        default = ociImage;
+        image = ociImage;
+        app = app;
+        poetry_env = poetry_env;
+      };
+      formatter = pkgs.alejandra;
+      devShells.default = poetry_env.env.overrideAttrs (old: {
+        buildInputs = [
+          app
+          pkgs.awscli2
+          pkgs.kubectl
+          pkgs.podman
+          pkgs.poetry
+        ];
       });
+    });
 }
-
